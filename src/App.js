7f4dbe4ddf7954607d858355ffbe6cbc4cb65f9e
Benton Washington <washingtonbenton@gmail.com>
2024-05-14 14:32:05 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <h1>Hello, my name is Benton! TEST TEST TEST</h1>
+
+    </div>
+  );
+}
+
+export default App;
+
+
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
